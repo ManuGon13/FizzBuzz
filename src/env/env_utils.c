169a0f8d58e03d048aa1d14a42d898b13eba01b1
egonin <egonin@f1br4s1.paris.42.school>
2026-03-12 11:47:56 +0100

    parsing J2

--- src/env/env_utils.c
+++ src/env/env_utils.c
@@ -6,9 +6,43 @@
 /*   By: egonin <egonin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/12 10:37:10 by egonin            #+#    #+#             */
-/*   Updated: 2026/03/12 10:54:43 by egonin           ###   ########.fr       */
+/*   Updated: 2026/03/12 11:47:21 by egonin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+char    *get_env_value(t_env *env, const char *key)
+{
+    while (env)
+    {
+        if (ft_strcmp(env->key, key) == 0)
+            return (env->value);
+        env = env->next;
+    }
+    return (NULL);
+}
+
+void    env_set(t_env **env, char *key, char *value)
+{
+    t_env   *tmp;
+    t_env   *new;
+
+    if (!env || !key || !value)
+        return ;
+    tmp = *env;
+    while (tmp)
+    {
+        if (ft_strcmp(tmp->key, key) == 0)
+        {
+            free(tmp->value);
+            tmp->value = ft_strdup(value);
+            return ;
+        }
+        tmp = tmp->next;
+    }
+    new = env_new(key, value);
+    if (!new)
+        return ;
+    env_add_back(env, new);
+}
