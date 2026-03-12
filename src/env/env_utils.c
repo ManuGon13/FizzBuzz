6493133a60f5624430472eb2238ecafd3db6a5a3
egonin <egonin@f1br4s1.paris.42.school>
2026-03-12 11:54:57 +0100

    parsing J2

--- src/env/env_utils.c
+++ src/env/env_utils.c
@@ -6,7 +6,7 @@
 /*   By: egonin <egonin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/12 10:37:10 by egonin            #+#    #+#             */
-/*   Updated: 2026/03/12 11:47:21 by egonin           ###   ########.fr       */
+/*   Updated: 2026/03/12 11:54:52 by egonin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,4 +45,38 @@ void    env_set(t_env **env, char *key, char *value)
     if (!new)
         return ;
     env_add_back(env, new);
+}
+
+void    env_delone(t_env *node)
+{
+    if (!node)
+        return;
+    free(node->key);
+    free(node->value);
+    free(node);
+}
+
+void    env_unset(t_env **env, char *key)
+{
+    t_env   *tmp;
+    t_env   *prev;
+
+    if (!env || !*env || !key)
+        return ;
+    tmp = *env;
+    prev = NULL;
+    while (tmp)
+    {
+        if (ft_strcmp(tmp->key, key) == 0)
+        {
+            if (prev == NULL)
+                *env = tmp->next;
+            else
+                prev->next = tmp->next;
+            env_delone(tmp);
+            return ;
+        }
+        prev = tmp;
+        tmp = tmp->next;
+    }
 }
